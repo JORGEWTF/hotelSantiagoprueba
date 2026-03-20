e63b836925e5ae1fe3c362c8fb32b20522c7bb8d
JORGEWTF <jorgejair.mfss.100@gmail.com>
2026-03-20 10:49:12 -0400

    fix: frontend production build with Nginx, fix vite hostname typo, update docker-compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['clienthotel.izzycorporation.com', 'localhost'],
+    allowedHosts: ['clientehotel.izzycorporation.com', 'localhost'],
   }
 })
